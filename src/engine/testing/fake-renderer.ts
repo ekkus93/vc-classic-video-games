1d9956ec21c347cc734d2d0835ac446a1ef31ce3
Phillip Chin <ekkus93@gmail.com>
2026-08-28 05:45:45 -0700

    P3: add headless renderer test double

---
+++ src/engine/testing/fake-renderer.ts
@@ -0,0 +1,21 @@
+import type { GameRenderer } from "../render/renderer.js";
+
+export class FakeGameRenderer implements GameRenderer {
+  public constructor(
+    public readonly logicalWidth = 320,
+    public readonly logicalHeight = 240,
+  ) {}
+
+  public clear(): void {}
+  public fillRect(): void {}
+  public strokeRect(): void {}
+  public drawLine(): void {}
+  public fillCircle(): void {}
+  public strokeCircle(): void {}
+  public drawText(): void {}
+  public drawSprite(): void {}
+  public save(): void {}
+  public restore(): void {}
+  public translate(): void {}
+  public rotate(): void {}
+}
