2ed89dfeb67eb08481532b039ead101bd1d1af93
Phillip Chin <ekkus93@gmail.com>
2026-08-28 05:18:35 -0700

    P2: publish core runtime API

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,1 +1,51 @@
+export type {
+  GameInstance,
+  GameModule,
+  GameRenderer,
+  GameStartOptions,
+} from "./game/contracts.js";
+export {
+  defineGameMetadata,
+  GameMetadataValidationError,
+  INPUT_KINDS,
+  parseGameMetadata,
+} from "./game/metadata.js";
+export type {
+  ControlDescription,
+  DifficultyDefinition,
+  GameMetadata,
+  InputKind,
+} from "./game/metadata.js";
+export {
+  DuplicateGameIdError,
+  GameRegistry,
+  UnknownGameIdError,
+} from "./game/registry.js";
+export {
+  GameLifecycle,
+  InvalidLifecycleTransitionError,
+} from "./game/lifecycle.js";
+export type { GameLifecycleState } from "./game/lifecycle.js";
+export {
+  ActiveGameRuntime,
+  InvalidRuntimeStateError,
+  NoActiveGameError,
+} from "./game/runtime.js";
+export type {
+  GameRuntimeErrorEvent,
+  GameRuntimeErrorHandler,
+  GameRuntimePhase,
+} from "./game/runtime.js";
+export type {
+  AssetService,
+  AudioService,
+  GameClock,
+  GameLogger,
+  GameServices,
+  GameStorageService,
+  InputService,
+  RandomService,
+  ScoreService,
+  ScoreSubmission,
+} from "./game/services.js";
 export { XorShift32 } from "./random/xorshift32.js";
