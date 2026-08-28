6e50d3e900b043904ac95d33528eda7265049cef
Phillip Chin <ekkus93@gmail.com>
2026-08-28 12:48:45 -0700

    P7-010: expose trusted shared audio unlock

--- src/app/shell/default-controller.ts
+++ src/app/shell/default-controller.ts
@@ -8,6 +8,7 @@ import {
   invokeNative,
   setApplicationFullscreen,
 } from "../../native/commands.js";
+import type { AudioUnlock } from "./audio-unlock-gesture.js";
 import { BrowserGameServices } from "./browser-game-services.js";
 import { ShellController } from "./controller.js";
 import { ShellGameInputBridge } from "./input-bridge.js";
@@ -17,6 +18,7 @@ export interface DefaultShellRuntime {
   readonly controller: ShellController;
   readonly gameHost: LoopingGameHost;
   readonly gameInput: ShellGameInputBridge;
+  readonly unlockAudio: AudioUnlock;
 }
 
 export function createDefaultShellRuntime(): DefaultShellRuntime {
@@ -41,8 +43,9 @@ export function createDefaultShellRuntime(): DefaultShellRuntime {
         fullscreen: { setFullscreen: setApplicationFullscreen },
       })
     : new ShellController(common);
+  const unlockAudio: AudioUnlock = () => gameServices.audio.unlock();
 
-  return Object.freeze({ controller, gameHost, gameInput });
+  return Object.freeze({ controller, gameHost, gameInput, unlockAudio });
 }
 
 export function createDefaultShellController(): ShellController {
